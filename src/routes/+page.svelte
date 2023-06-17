8bcb757785f8aaaae99297fafe8d91566627e581
Inveracity <cbaklid@gmail.com>
2023-06-17 07:48:36 +0200

    add more ui elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import {Subscribe} from '../grpc'
+    import {notifier} from '../store'
 
-<p><a href="/about">about</a></p>
+    let subscriberId = "joe"
+
+</script>
+
+<div>
+    <h1>GRPC Stream</h1>
+
+    <div>
+        <p>Click the Subscribe button receive realtime events</p>
+    </div>
+
+    <div>
+        <input bind:value={subscriberId} placeholder="subscriber id" />
+        <br />
+        <button on:click={Subscribe}> Subscribe </button>
+        <button on:click={notifier.reset}> clear </button>
+    </div>
+
+    <div>
+        {#each $notifier as item}
+            <p>{item}</p>
+        {/each}
+    </div>
+</div>
+
+
+<style>
+    div {
+        margin: 10px;
+    }
+    input {
+        padding: 2px;
+        margin: 10px;
+    }
+    button {
+        padding: 2px;
+        margin: 10px;
+    }
+    p {
+        margin: 10px;
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
