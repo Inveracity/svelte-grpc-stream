c6ffb173fedab395f77135c2b1f0ab731d7f1b0f
Inveracity <cbaklid@gmail.com>
2023-06-20 18:08:27 +0200

    wip

--- api/queue.py
+++ api/queue.py
@@ -1,8 +1,12 @@
 import nats
 from api.models.events import Event
+from os import environ
+
+NATS_HOST = environ.get('NATS_HOST', '127.0.0.1')
+NATS_PORT = environ.get('NATS_PORT', '4222')
 
 async def send(evt: Event):
-    nc = await nats.connect("nats://nats:4222")
+    nc = await nats.connect(f"nats://{NATS_HOST}:{NATS_PORT}")
     js = nc.jetstream()
     
     await js.publish(
