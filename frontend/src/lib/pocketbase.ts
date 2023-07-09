ae43c49d733fc47180b7a854ccbe14562ca5bb44
Inveracity <cbaklid@gmail.com>
2023-07-09 22:22:45 +0200

    wip channel persistence

--- frontend/src/lib/pocketbase.ts
+++ frontend/src/lib/pocketbase.ts
@@ -1,13 +1,34 @@
 import { env } from '$env/dynamic/public';
-
 import PocketBase from 'pocketbase';
-
 import { writable } from 'svelte/store';
+import { channels } from './stores/channel';
+import { users } from './stores/users';
 
 export const pb = new PocketBase(env.PUBLIC_POCKETBASE_URL);
-
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange(() => {
   currentUser.set(pb.authStore.model);
 });
+
+export const fetchChannels = async () => {
+  const records = await pb.collection('channels').getFullList({
+    sort: 'created',
+  });
+
+  // convert records to array and set in channels store
+  channels.set(records.map((record) => {
+    return record.name;
+  }));
+}
+
+export const fetchUsers = async () => {
+  const records = await pb.collection('users').getFullList({
+    sort: 'created',
+  });
+
+  // convert records to array and set in channels store
+  users.set(records.map((record) => {
+    return record.name;
+  }));
+}
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
 	import '../app.css';
 	import { onDestroy, onMount } from 'svelte';
-	import { currentUser, pb } from '$lib/pocketbase';
+	import { currentUser, fetchChannels, fetchUsers, pb } from '$lib/pocketbase';
 	import { Connect, Disconnect } from '$lib/grpc';
 	import { server } from '$lib/stores/server';
 
 	onMount(() => {
 		if ($currentUser) {
+			fetchChannels();
+			fetchUsers();
 			Connect($server, $currentUser.username, '0', pb.authStore.token);
 		}
 	});
