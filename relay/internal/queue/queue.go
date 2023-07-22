86d341b07bb6bb23023bc023d7f446c5133e91a8
Inveracity <cbaklid@gmail.com>
2023-07-22 12:41:20 +0200

    hacky fix for racecondition

--- relay/internal/queue/queue.go
+++ relay/internal/queue/queue.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/nats-io/nats.go"
 )
@@ -48,6 +49,7 @@ func (q *Queue) Subscribe(ctx context.Context, channel string) error {
 		// either the context is cancelled or an error was caught on the error channel
 		// Currently unsure why.
 		case <-ctx.Done():
+			time.Sleep(1 + time.Second) // TODO: There's a race condition when a client disconnects and the server wants to broadcast a message.
 			sub.Unsubscribe()
 			log.Printf("NATS %s: unsubscribing and closing stream", q.streamid)
 			q.Close()
